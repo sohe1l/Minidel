5136006e2fd4eb58985123cfb7f083b502b711ea
= <=>
2015-12-07 21:31:42 +0400

    added inline images

--- resources/views/manage/inline.blade.php
+++ resources/views/manage/inline.blade.php
@@ -27,6 +27,64 @@
 
     <textarea class="form-control" style="height:100px;"><iframe src="http://www.minidel.com/{{$store->slug}}/order/inline" style="width:100%; height:900px;"><a href="http://www.minidel.com/{{$store->slug}}/order/inline" target="_blank">Online Order now from {{$store->name}}</a></iframe></textarea>
 
+    <br>
+
+    <h2><small>You can also link directly to your order page through the following order buttons.</small></h2>
+    <table class="table" style="text-align: center">
+      <tr>
+        <td style="width:50%">
+          <textarea class="form-control" style="width:100%;"><a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel1.jpg"></a></textarea>
+        </td>
+        <td style="width:50%">
+          <a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel1.jpg"></a>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width:50%">
+          <textarea class="form-control" style="width:100%;"><a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel2.jpg"></a></textarea>
+        </td>
+        <td style="width:50%">
+          <a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel2.jpg"></a>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width:50%">
+          <textarea class="form-control" style="width:100%;"><a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel3.png"></a></textarea>
+        </td>
+        <td style="width:50%">
+          <a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel3.png"></a>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width:50%">
+          <textarea class="form-control" style="width:100%;"><a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel4.png"></a></textarea>
+        </td>
+        <td style="width:50%">
+          <a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel4.png"></a>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width:50%">
+          <textarea class="form-control" style="width:100%;"><a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel5.jpg"></a></textarea>
+        </td>
+        <td style="width:50%">
+          <a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel5.jpg"></a>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width:50%">
+          <textarea class="form-control" style="width:100%;"><a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel6.png"></a></textarea>
+        </td>
+        <td style="width:50%">
+          <a href="http://www.minidel.com/{{$store->slug}}/order/" target="_blank"><img title="Order now from {{$store->name}}" src="http://www.minidel.com/img/buttons/minidel6.png"></a>
+        </td>
+      </tr>
+    </table>
 
   </div>
 </div>
