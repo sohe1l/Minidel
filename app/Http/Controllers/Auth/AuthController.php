7ecc31bd225beb19b7c928b10c43f0c181b8ce6c
= <=>
2015-12-09 14:22:38 +0400

    added confirm email

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'getLogout']);
+        $this->middleware('guest', ['except' => ['getLogout','confrimEmail']]);
     }
 
 
